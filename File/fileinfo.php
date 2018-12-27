9caa817aeb8ef594958d786e22e99c2e57e65ffb
liaoyuehuan <1309893442@qq.com>
2018-12-27 14:20:27 +0800

    合并

--- File/fileinfo.php
+++ File/fileinfo.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hj
- * Date: 2018/11/29
- * Time: 17:57
- */
+$fInfo = new finfo();
+echo $fInfo->file(__DIR__.'/fileinfo_files/test.pdf').PHP_EOL; //PDF document, version 1.4
+
+$fInfo = new finfo(FILEINFO_MIME_TYPE);
+echo $fInfo->file(__DIR__.'/fileinfo_files/test.pdf').PHP_EOL; //application/pdf
+
+#$fInfo = new finfo(FILEINFO_EXTENSION );
+#echo $fInfo->file(__DIR__.'/fileinfo_files/test.pdf').PHP_EOL; // PHP 7.2.0 起有效。
+
+echo mime_content_type(__DIR__.'/fileinfo_files/test.pdf').PHP_EOL;; //application/pdf
+echo mime_content_type(__DIR__.'/fileinfo_files/test.png').PHP_EOL;; //image/png
+echo mime_content_type(__DIR__.'/fileinfo_files/test.txt').PHP_EOL;; //text/plain
+
