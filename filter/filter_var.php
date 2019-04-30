5b90f42bfb99cf6e70f3b4fb7dc86285537a8aec
liaoyuehuan <1309893442@qq.com>
2019-04-30 18:02:34 +0800

    1.增加pdflib

--- filter/filter_var.php
+++ filter/filter_var.php
@@ -1,10 +1,20 @@
 <?php
 $email = '1309893442@q#q.com';
-$email = filter_var($email,FILTER_SANITIZE_EMAIL);
-echo $email.PHP_EOL;
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+echo $email . PHP_EOL;
 
-$validate = filter_var($email,FILTER_VALIDATE_EMAIL);
-var_dump( $validate);
+$validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+echo 'FILTER_VALIDATE_EMAIL : ' . $validate . PHP_EOL;
+
+$ip = '127.0.0.1';
+$validate = filter_var($ip, FILTER_VALIDATE_IP);
+echo 'FILTER_VALIDATE_IP : ' . $validate . PHP_EOL;
+
+
+$url = 'https://www.pdflib.com/download/pdflib-product-family/';
+$validate = filter_var($ip, FILTER_VALIDATE_URL);
+echo 'FILTER_VALIDATE_URL : ' . $validate . PHP_EOL;
+
 
 #参考链接 ：  https://blog.csdn.net/fafa211/article/details/5054452
 #
---
+++ pdf/pdflib/two_page.php
@@ -0,0 +1,46 @@
+<?php
+try {
+    $p = PDF_new();
+    $filename = __DIR__ . '/files/two_page.pdf';
+    $optionList = '';
+    if (PDF_begin_document($p, $filename, $optionList) == 0) {
+        die("Error: " . PDF_get_errmsg($p));
+    }
+
+    PDF_set_info($p, 'Creator', 'php');
+    PDF_set_info($p, 'Author', 'liaoyuehuan');
+    PDF_set_info($p, 'Title', 'hello PDF');
+
+    # first page
+    PDF_begin_page_ext($p, 595, 842, '');
+    $font = PDF_load_font($p, "Helvetica-Bold", "winansi", "");
+    PDF_setfont($p, $font, 24);
+    PDF_set_text_pos($p, 100, 600);
+    PDF_show($p, 'hi pdf girl');
+    PDF_continue_text($p, 'continue');
+    PDF_end_page_ext($p, '');
+    # end first page
+
+    # second page
+    PDF_begin_page_ext($p, 595, 842, '');
+    $font = PDF_load_font($p, "Helvetica-Bold", "winansi", "");
+    PDF_setfont($p, $font, 24);
+    PDF_set_text_pos($p, 100, 600);
+    PDF_show($p, 'hi pdf girl');
+    PDF_continue_text($p, 'continue');
+    PDF_end_page_ext($p, '');
+    # end second page
+
+    PDF_end_document($p, '');
+    if ($filename) {
+        echo 'success';
+    } else {
+        $buf = PDF_get_buffer($p); // $filename empty
+        var_dump($buf);
+    }
+    PDF_delete($p);
+} catch (PDFLibException $e) {
+    var_dump($e);
+} catch (Exception $e) {
+    var_dump($e);
+}
