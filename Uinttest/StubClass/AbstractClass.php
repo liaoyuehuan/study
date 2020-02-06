156114ee1861f85b57fbfb7f85efbabc081f9c13
liaoyuehaun <1309893442@qq.com>
2020-02-06 12:05:34 +0800

    1.提交代码

---
+++ Uinttest/StubClass/AbstractClass.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/1/2 0002
+ * Time: 23:26
+ */
+
+namespace Uinttest\StubClass;
+
+
+abstract class AbstractClass
+{
+    abstract public function say();
+}
---
+++ Uinttest/StubClass/TestTrait.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/1/2 0002
+ * Time: 23:06
+ */
+
+namespace Uinttest\StubClass;
+
+
+trait TestTrait
+{
+    public function say()
+    {
+        return true;
+    }
+}
